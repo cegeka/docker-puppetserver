0d3090e889c4fd269b9723d245b9554af2768dc8
Thomas Meeus <thomas@sector7g.be>
2018-04-20 12:53:15 +0200

    New init script

--- scripts/puppetserver.sh
+++ scripts/puppetserver.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-# default variables
-JAVA_ARGS="${JAVA_ARGS:--Xms2g -Xmx2g}"
+LOG_APPENDER="-Dlogappender=STDOUT"
 
-# copied from SystemD unit provided by puppet server
-exec /usr/bin/java -Xms512m -Xmx1g -Djruby.logger.class=com.puppetlabs.jruby_utils.jruby.Slf4jLogger -Djava.security.egd=/dev/urandom '-XX:OnOutOfMemoryError=kill -9 %%p' -cp /opt/puppetlabs/server/apps/puppetserver/puppet-server-release.jar:/opt/puppetlabs/server/apps/puppetserver/jruby-1_7.jar:/opt/puppetlabs/server/data/puppetserver/jars/* clojure.main -m puppetlabs.trapperkeeper.main --config /etc/puppetlabs/puppetserver/conf.d --bootstrap-config /etc/puppetlabs/puppetserver/services.d/,/opt/puppetlabs/server/apps/puppetserver/config/services.d/ --restart-file /opt/puppetlabs/server/data/puppetserver/restartcounter foreground
+source /etc/sysconfig/puppetserver
+
+/usr/bin/java ${JAVA_ARGS} ${LOG_APPENDER} \
+         -cp ${INSTALL_DIR}/puppet-server-release.jar \
+         clojure.main -m puppetlabs.trapperkeeper.main \
+         --config ${CONFIG} --bootstrap-config ${BOOTSTRAP_CONFIG} \
+         ${@}
