c1e447ef4b71a458b2d519f1055005f35d9ba0be
thomasmeeus <thomas@sector7g.be>
2019-01-09 16:38:53 +0100

    Working Jenkins configuration

---
+++ jenkins_configuration/config.groovy
@@ -0,0 +1,63 @@
+import static jenkins.model.Jenkins.instance as jenkins
+
+import jenkins.branch.OrganizationFolder
+import org.jenkinsci.plugins.github_branch_source.BranchDiscoveryTrait
+import org.jenkinsci.plugins.github_branch_source.GitHubSCMNavigator
+import org.jenkinsci.plugins.github_branch_source.OriginPullRequestDiscoveryTrait
+try {
+
+    def githubOrg = 'cegeka'
+
+    // Delete Openshift demo-job
+    def matchedJobs = jenkins.items.findAll { job ->
+      job.name =~ /OpenShift Sample/
+    }
+
+    if (!matchedJobs.isEmpty()) {
+        println "--> Deleting following jobs:"
+        matchedJobs.each { job ->
+            println job.name
+            job.delete()
+        }
+    }
+
+    // Configure Github Branch Source plugin
+    println '--> Creating organization folder'
+    // Create the top-level item if it doesn't exist already.
+    def folder = jenkins.items.isEmpty() ? jenkins.createProject(OrganizationFolder, 'MyName') : jenkins.items[0]
+    // Set up GitHub source.
+    def navigator = new GitHubSCMNavigator(githubOrg)
+    navigator.credentialsId = 'ci00053160-puppetserver-github-credentials' // Loaded above in the GitHub section.
+
+    navigator.traits = [
+        // Too many repos to scan everything. This trims to a svelte 265 repos at the time of writing.
+        new jenkins.scm.impl.trait.WildcardSCMSourceFilterTrait('puppet-monorepo', ''),
+        // We have a ton of old branches so try to limit to just master and PRs for now.
+        new jenkins.scm.impl.trait.RegexSCMHeadFilterTrait('^(master|PR-.*)'),
+        new BranchDiscoveryTrait(1), // Exclude branches that are also filed as PRs.
+        new OriginPullRequestDiscoveryTrait(1), // Merging the pull request with the current target branch revision.
+    ]
+
+    folder.navigators.replace(navigator)
+
+    println '--> Saving Jenkins config'
+    jenkins.save()
+
+    println '--> Scheduling GitHub organization scan'
+
+    Thread.start {
+        sleep 30000 // 30 seconds
+        println '--> Running GitHub organization scan'
+        folder.scheduleBuild()
+    }
+
+    println "--> Configuration of jenkins is done"
+}
+catch(Throwable exc) {
+    println '!!! Error configuring jenkins'
+    org.codehaus.groovy.runtime.StackTraceUtils.sanitize(new Exception(exc)).printStackTrace()
+    println '!!! Shutting down Jenkins to prevent possible mis-configuration from going live'
+    jenkins.cleanUp()
+    System.exit(1)
+}
+
--- ocp/jenkins.sh
+++ ocp/jenkins.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
 
-oc new-build  -D $'FROM jenkins:latest\n
+## Use latest Jenkins container to fix credential-sync-plugin
+oc import-image jenkins-2-rhel7 --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.11.51-2 --confirm
+
+## Customize the the image imported above with all the build tools we need
+oc new-build -D $'FROM jenkins-2-rhel7:latest \n
       USER root\nRUN yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && yum install -y python-setuptools puppet rubygem-puppet-lint && yum clean all && easy_install pip && pip install yamllint\n
       USER 1001' --name=puppet-jenkins
 
+## Define Jenkins customization in config map
+oc create configmap jenkins-configuration --from-file=casc_jenkins.yaml=../jenkins_configuration/casc_jenkins.yaml --from-file=config.groovy=../jenkins_configuration/config.groovy
 
-## to fix:
-JENKINS_PLUGINS="GitHub+Branch+Source+Plugin:2.4.1"
+## Set of additional plugins to install. Github branch source plugin is installed by default
+JENKINS_PLUGINS="configuration-as-code"
 
-oc new-app jenkins -p MEMORY_LIMIT=1Gi ENABLE_OAUTH=true DISABLE_ADMINISTRATIVE_MONITORS=true INSTALL_PLUGINS="${JENKINS_PLUGINS}"
-oc patch dc jenkins -p '{"spec": {"template": {"spec": {"containers": [{"name": "jenkins","resources": {"requests": {"cpu": "1", "memory": "1Gi"}, "limits": {"cpu": "1", "memory": "1Gi}}}]}}}}'
+## Deploy the Openshift built-in Jenkins template with the newly build image.
+oc process openshift//jenkins-ephemeral -p JENKINS_IMAGE_STREAM_TAG=puppet-jenkins:latest NAMESPACE=ci00053160-puppetserver | oc create -f -
+
+## Pause rollouts to proceed with additional configuration
+oc rollout pause dc jenkins
+
+## Up memory & cpu to get a responsive Jenkins
+oc patch dc jenkins -p '{"spec":{"template":{"spec":{"containers":[{"name":"jenkins","resources":{"requests":{"cpu":"1","memory":"1Gi"},"limits":{"cpu":"1","memory":"1Gi"}}}]}}}}'
+oc set env dc/jenkins MEMORY_LIMIT=1Gi
+
+oc set env dc/jenkins DISABLE_ADMINISTRATIVE_MONITORS=true
+oc set env dc/jenkins INSTALL_PLUGINS="${JENKINS_PLUGINS}"
+oc set env dc/jenkins CASC_JENKINS_CONFIG="/var/lib/jenkins/init.groovy.d/casc_jenkins.yaml"
+oc set volumes dc/jenkins --add --configmap-name=jenkins-configuration --mount-path='/var/lib/jenkins/init.groovy.d/'
+
+oc rollout resume dc jenkins
 oc expose svc/jenkins
-
-
-
-
