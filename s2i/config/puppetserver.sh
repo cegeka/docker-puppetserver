3963e05cd55482e5965f7f432a5f38039475adf4
andreias <andrei.asan@cegeka.com>
2019-12-04 17:18:39 +0200

    add facter.jar to java start

--- s2i/config/puppetserver.sh
+++ s2i/config/puppetserver.sh
@@ -7,7 +7,7 @@ LOG_APPENDER="-Dlogappender=STDOUT"
 source /etc/sysconfig/puppetserver
 
 /usr/bin/java ${JAVA_ARGS} ${LOG_APPENDER} \
-         -cp ${INSTALL_DIR}/puppet-server-release.jar:${INSTALL_DIR}/jruby-9k.jar:${INSTALL_DIR}/jars/* \
+         -cp ${INSTALL_DIR}/puppet-server-release.jar:${INSTALL_DIR}/jruby-9k.jar:/opt/puppetlabs/puppet/lib/ruby/vendor_ruby/facter.jar:${INSTALL_DIR}/jars/* \
          clojure.main -m puppetlabs.trapperkeeper.main \
          --config ${CONFIG} --bootstrap-config ${BOOTSTRAP_CONFIG} \
          ${@}
